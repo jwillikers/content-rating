b4732cb8d6712a7449998ee493b405b51e0cb0ea
Jordan Williams <jordanwilliams@solutionary.com>
2018-02-19 13:47:42 -0600

    removed ordering for models
    Ordering caused an error and is also very inefficient, so ordering will
    not be used by default.

--- capstoneproject/models.py
+++ capstoneproject/models.py
@@ -8,9 +8,6 @@ class Category(models.Model):
     def __str__(self):
         return u'%s %d' % (self.category, self.weight)
 
-    class Meta:
-        ordering = ['category']
-
 
 class Word(models.Model):
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -20,9 +17,6 @@ class Word(models.Model):
     def __str__(self):
         return u'%s %s %d' % (self.category, self.word, self.weight)
 
-    class Meta:
-        ordering = ['category', 'word']
-
 
 class Spelling(models.Model):
     word_or_phrase = models.ForeignKey(Word, on_delete=models.CASCADE)
@@ -30,9 +24,6 @@ class Spelling(models.Model):
 
     def __str__(self):
         return u'Word or phrase: %s \nSpelling: %s' % (self.word_or_phrase, self.spelling)
-
-    class Meta:
-        ordering = ['word_or_phrase', 'spelling']
 
 
 class Phrase(models.Model):
@@ -43,6 +34,3 @@ class Phrase(models.Model):
 
     def __str__(self):
         return u'%s %s' % (self.id, self.spelling)
-
-    class Meta:
-        ordering = ['category', 'phrase', 'weight', 'word']
