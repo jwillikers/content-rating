ba28d62fcd81a2fdf8e38f6451b20ecfda8b85d0
Jordan Williams <jordanwilliams@solutionary.com>
2018-02-19 11:50:26 -0600

    set up default dictionary and user settings models
    Deleted commented-out user model because we can use the default Django
    user model for users' settings. The default dictionary models, i.e. the
    phrase, word, spelling, and category models, were created without user
    id's, since we still want to export the user dictionary tables off of
    the website. The phrase table has only a single word id because word_set
    can be used to link to multiple words according to the ForeignKey
    construct.

--- capstoneproject/models.py
+++ capstoneproject/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 
-# I was just testing some stuff out, Jordan. Feel free to make changes.
-
 class Category(models.Model):
     category = models.CharField(max_length=30)
     weight = models.IntegerField()
@@ -13,26 +11,38 @@ class Category(models.Model):
     class Meta:
         ordering = ['category']
 
+
 class Word(models.Model):
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     word = models.CharField(max_length=30)
     weight = models.IntegerField()
 
     def __str__(self):
-        return u'%s %d' % (self.word, self.weight)
+        return u'%s %s %d' % (self.category, self.word, self.weight)
 
     class Meta:
-        ordering = ['word', 'category_id']
+        ordering = ['category', 'word']
 
-'''
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
-    password_hash = models.CharField(max_length=30)
+
+class Spelling(models.Model):
+    word_or_phrase = models.ForeignKey(Word, on_delete=models.CASCADE)
+    spelling = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.username
+        return u'Word or phrase: %s \nSpelling: %s' % (self.word_or_phrase, self.spelling)
 
     class Meta:
-        ordering = ['username', 'email']
-'''
+        ordering = ['word_or_phrase', 'spelling']
+
+
+class Phrase(models.Model):
+    phrase = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE())
+    weight = models.IntegerField()
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return u'%s %s' % (self.id, self.spelling)
+
+    class Meta:
+        ordering = ['category', 'phrase', 'weight', 'word']
