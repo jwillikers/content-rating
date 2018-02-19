77b02786447af42e7a78aec4596b628d7cba6c1e
Jordan Williams <jordanwilliams@solutionary.com>
2018-02-19 13:20:04 -0600

    Revert "removed dev env"
    This reverts commit e99dbb1506444675f6aac4cf4550f3c3bfc5ea24.

---
+++ development_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mulis\onedrive\docume~1\classe~1\cscaps~1\conten~1\develo~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
