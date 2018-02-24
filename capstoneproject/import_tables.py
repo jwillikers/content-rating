408f7ea410d0e6ff0e239f15797c3eba9021a83e
Jordan Williams <jordanwilliams@solutionary.com>
2018-02-23 23:41:26 -0600

    import_tables works for all tables now
    I also modified the models to allow the phrases table to have a
    ManyToManyField which allows for one word field which connects to many
    fields

--- capstoneproject/import_tables.py
+++ capstoneproject/import_tables.py
@@ -1,78 +1,162 @@
 import csv
 import os
 import django
+from django.core.exceptions import ObjectDoesNotExist
+from sys import argv
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 django.setup()
 
 # your imports, e.g. Django models
-import capstoneproject.models
+from capstoneproject.models import Category, Word, Phrase, PhraseSpelling, WordSpelling
+
+categories = Category.objects
+words = Word.objects
+phrases = Phrase.objects
+word_spellings = WordSpelling.objects
+phrase_spellings = PhraseSpelling.objects
+
+
+def csv_reader(path):
+    f = open(path, 'r')
+    print('opened ' + path.rpartition('/')[2])
+    reader = csv.DictReader(f)
+    print('header fields:')
+    for name in reader.fieldnames:
+        print('\t' + name)
+    return reader
 
 
 def import_category(path):
-    f = open(path, 'r')
-    reader = csv.reader(f)
+    reader = csv_reader(path)
 
-    for row in reader:
-        row0 = row[0]
-        if row0 != 'category':
-            _, created = capstoneproject.models.Category.objects.get_or_create(
-                category=row0,
-                weight=row[1],
-            )
+    for category_entry in reader:
+        try:
+            category = categories.get(category=category_entry['category'])
+        except ObjectDoesNotExist:
+            category = Category(category=category_entry['category'])
+
+        try:
+            category.weight = category_entry['weight']
+        except ValueError:
+            print('skipping ' + category_entry['category']
+                  + ': weight ' + category_entry['weight'] + ' is not an integer')
+
+        category.save()
+
+    print('import of csv into category table complete\n')
 
 
 def import_word(path):
-    f = open(path);
-    reader = csv.reader(f)
+    reader = csv_reader(path)
 
-    for row in reader:
-        row0 = row[0]
-        if row0 != 'category':
-            _, created = capstoneproject.models.Word.objects.get_or_create(
-                category=capstoneproject.models.Category.objects.get(pk=row[0]),
-                word=row[1],
-                weight=row[2],
-            )
+    for word_entry in reader:
+        try:
+            category = categories.get(category=word_entry['category'])
+            words.update_or_create(word=word_entry['word'], category_id=category.id, weight=word_entry['weight'])
+        except ObjectDoesNotExist:
+            print('skipping ' + word_entry['word'] + ': category \'' + word_entry['category'] + '\' does not exist')
+        except ValueError:
+            print('skipping ' + word_entry['word'] + ': weight ' + word_entry['weight'] + ' is not an integer')
+
+    print('import of csv into words table complete\n')
 
 
 def import_phrase(path):
-    f = open(path)
-    reader = csv.reader(f)
+    reader = csv_reader(path)
 
-    for row in reader:
-        row0 = row[0]
+    for phrase_entry in reader:
+        try:
+            category = categories.get(category=phrase_entry['category'])
+        except ObjectDoesNotExist:
+            print('skipping ' + phrase_entry['phrase'] + ': category \'' + phrase_entry['category'] + '\' does not exist')
+            continue
 
-        row3 = row[3]
-        if row3 == '':
-            word_obj = None
-        else:
-            word_obj = capstoneproject.models.Word.objects.get(pk=row3)
+        word_set = list()
+        for word_word in [phrase_entry['word1'], phrase_entry['word2'], phrase_entry['word3']]:
+            if word_word != '':
+                try:
+                    word_set.append(words.get(word=word_word))
+                except ObjectDoesNotExist:
+                    print('skipping ' + phrase_entry['phrase'] + ': word \'' + word_word + '\' does not exist')
 
-        if row0 != 'category':
-            _, created = capstoneproject.models.Phrase.objects.get_or_create(
-                category=capstoneproject.models.Category.objects.get(pk=row0),
-                phrase=row[1],
-                weight=row[2],
-                word=word_obj,
-            )
+        try:
+            phrase = phrases.get(phrase=phrase_entry['phrase'])
+        except ObjectDoesNotExist:
+            phrase = Phrase()
+
+        phrase.category_id = category.id
+        phrase.phrase = phrase_entry['phrase']
+        phrase.weight = phrase_entry['weight']
+        phrase.word.clear()
+        for word_obj in word_set:
+            phrase.word.add(word_obj)
+        phrase.save()
+
+    print('import of csv into phrases table complete\n')
+
+
+def import_phrase_spelling(path):
+    reader = csv_reader(path)
+
+    for spelling_entry in reader:
+        try:
+            phrase = phrases.get(phrase=spelling_entry['phrase'])
+        except ObjectDoesNotExist:
+            print('skipping ' + spelling_entry['spelling']
+                  + ': phrase \'' + spelling_entry['phrase'] + '\' does not exist')
+            continue
+
+        phrase_spellings.update_or_create(
+            phrase_id=phrase.id,
+            spelling=spelling_entry['spelling'],
+        )
+
+    print('import of csv into phrases spelling table complete\n')
 
 
 def import_word_spelling(path):
-    f = open(path)
-    reader = csv.reader(f)
+    reader = csv_reader(path)
 
-    for row in reader:
-        row0 = row[0]
-        if row0 != 'word':
-            _, created = capstoneproject.models.WordSpelling.objects.get_or_create(
-                word=capstoneproject.models.Word.objects.get(pk=row0),
-                spelling=row[1],
-            )
+    for spelling_entry in reader:
+        try:
+            word = words.get(word=spelling_entry['word'])
+        except ObjectDoesNotExist:
+            print('skipping ' + spelling_entry['spelling'] + ': word \'' + spelling_entry['word'] + '\' does not exist')
+            continue
+
+        word_spellings.update_or_create(
+            word_id=word.id,
+            spelling=spelling_entry['spelling'],
+        )
+
+    print('import of csv into words spelling table complete\n')
+
+
+def import_tables(root_folder='', category_path='', word_path='', phrase_path='', wordspelling_path='',
+                  phrasespelling_path=''):
+    if category_path != '':
+        import_category(root_folder + category_path)
+    if word_path != '':
+        import_word(root_folder + word_path)
+    if phrase_path != '':
+        import_phrase(root_folder + phrase_path)
+    if wordspelling_path != '':
+        import_word_spelling(root_folder + wordspelling_path)
+    if phrasespelling_path != '':
+        import_phrase_spelling(root_folder + phrasespelling_path)
+    print('\nall imports complete\n')
 
 
 def main():
-    import_word_spelling("/Users/jwilliams/Downloads/WordSpelling.csv")
+    import_tables(
+        root_folder='/Users/jwilliams/Downloads/',
+        category_path='Category.csv',
+        word_path='Word.csv',
+        phrase_path='Phrase.csv',
+        wordspelling_path='WordSpelling.csv',
+        phrasespelling_path='PhraseSpelling.csv'
+    )
 
 
 if __name__ == '__main__':
--- capstoneproject/models.py
+++ capstoneproject/models.py
@@ -2,51 +2,43 @@ from django.db import models
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=30)
+    category = models.CharField(unique=True, max_length=30)
     weight = models.IntegerField()
 
     def __str__(self):
-        return u'Category: %s\n' \
-               u'Weight  :  %d\n' % (self.category, self.weight)
+        return self.category
 
 
 class Word(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    word = models.CharField(max_length=30)
+    word = models.CharField(unique=True, max_length=30)
     weight = models.IntegerField()
 
     def __str__(self):
-        return u'Category: %s\n' \
-               u'Word    : %s\n' \
-               u'Weight  : %d\n' % (self.category, self.word, self.weight)
+        return self.word
 
 
 class Phrase(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    phrase = models.CharField(max_length=100)
+    phrase = models.CharField(unique=True, max_length=100)
     weight = models.IntegerField()
-    word = models.ForeignKey(Word, on_delete=models.CASCADE, null=True)
+    word = models.ManyToManyField(Word, null=True)
 
     def __str__(self):
-        return u'Category : %s\n' \
-               u'Phrase   : %s\n' \
-               u'Weight   : %d\n' \
-               u'Word     : %s\n' % (self.category, self.phrase, self.weight, self.word)
+        return self.phrase
 
 
 class WordSpelling(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    spelling = models.CharField(max_length=30)
+    spelling = models.CharField(unique=True, max_length=30)
 
     def __str__(self):
-        return u'Word    : %s\n' \
-               u'Spelling: %s\n' % (self.word, self.spelling)
+        return self.spelling
 
 
 class PhraseSpelling(models.Model):
     phrase = models.ForeignKey(Phrase, on_delete=models.CASCADE)
-    spelling = models.CharField(max_length=100)
+    spelling = models.CharField(unique=True, max_length=100)
 
     def __str__(self):
-        return u'Phrase  : %s\n' \
-               u'Spelling: %s\n' % (self.phrase, self.spelling)
+        return self.spelling
