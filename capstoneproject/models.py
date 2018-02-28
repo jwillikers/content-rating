9e49f39792039b9a568d4c8285b8f438da872b73
Jordan Williams <jordanwilliams@solutionary.com>
2018-02-27 21:51:21 -0600

    added subcategories in the form of parent field
    Subcategories will be very helpful for avoiding duplication within the
    databases. Since there will be a table for each category, words such as
    sexual orientation slurs would be duplicated in multiple tables, when
    they don't need to be. A strong sexual slur word would always be a
    strong slur word. However, the change is to a many-to-many relationship,
    so it would follow that a strong sexual slur word would always be a
    strong sensual word if that was indicated as a parent. This may not be
    desired.

--- capstoneproject/models.py
+++ capstoneproject/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     category = models.CharField(unique=True, max_length=30)
+    parent_category = models.ManyToManyField('self', null=True)
     weight = models.IntegerField()
 
     def __str__(self):
