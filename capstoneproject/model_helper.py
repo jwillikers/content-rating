136e29d241b64f50a989e21c7f289e897a3e4743
Craig Olson <cdolson@unomaha.edu>
2018-04-04 09:55:34 -0500

    Added a model_helper class to perform database queries

--- capstoneproject/model_helper.py
+++ capstoneproject/model_helper.py
@@ -1,29 +1,40 @@
 """
-This file contains functions used to provide data from the database to display on the UI.
+This file contains functions used to provide data from the database.
 """
-from capstoneproject import model_helper
+from capstoneproject.models import Category
+from capstoneproject.models import Word
+import traceback
 
 
-def display_categories():
+def get_categories():
     """
     This function returns all categories that are used to classify offensive content.
     :return: A list of categories stored in the database.
     """
-    return model_helper.get_categories()
+    return Category.categories.all()
 
 
-def display_words():
+def get_words():
     """
     This function returns all words that are used to classify offensive content.
     :return: A list of all words stored in the system's dictionary of offensive content.
     """
-    return model_helper.get_words()
+    return Word.words.all()
 
 
-def display_category_words(category):
+def get_category_words(category):
     """
     This function returns all words that are used to classify offensive content from the given category.
     :param category: The offensive category of words that should be returned.
     :return: A list of words in the given category stored in the system's dictionary of offensive content.
     """
-    return model_helper.get_category_words(category)
+    return Word.words.category(category)
+
+
+def get_word(word):
+    word_model = None
+    try:
+        word_model = Word.words.get_word(word=word)
+    except TypeError:
+        print(traceback.print_exc())
+    return word_model
