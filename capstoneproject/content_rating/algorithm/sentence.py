743d6e077f51eebc9ad7726b0d658ce73664a37a
Craig Olson <cdolson@unomaha.edu>
2018-04-04 09:57:43 -0500

    Updated sentence.py class to use the new database functions

--- capstoneproject/content_rating/algorithm/sentence.py
+++ capstoneproject/content_rating/algorithm/sentence.py
@@ -2,25 +2,14 @@
 This file contains the Sentence class which contains data on individual sentences within a given text.
 """
 import nltk
-from capstoneproject.models import Category, Word
-from collections import defaultdict
+from capstoneproject.models import Word, WordFeatureQuerySet, WordFeature, WordQuerySet
+from capstoneproject import model_helper
 
 
 class Sentence:
     """
     The class represents a sentence within the text to be classified and rated.
     """
-    '''
-    sentence_number = 0  # Location of the sentence within the original text.
-    number_of_clean_words = 0      # Number of clean words within the sentence.
-    number_of_offensive_words = 0  # Number of offensive words within the sentence.
-    sentence_tokens = []  # List of tuples in the form of (word, part of speech tag) in the sentence.
-    strongly_offensive_words = defaultdict(int)  # Dictionary containing strongly offensive word/category pairs and
-    # the number of occurrences in the sentence.
-    weakly_offensive_words = defaultdict(int)    # Dictionary containing weakly offensive word/category pairs and the
-    # number of occurrences in the sentence.
-    offensive_categories = []                    # List containing the offensive categories met by this sentence
-    '''
 
     def __init__(self, sentence: list, number: int):
         """
@@ -28,20 +17,41 @@ class Sentence:
         :param sentence: A list of (word, part of speech tag) tuples of the sentence represented.
         :param number: The position in the original text that this sentence can be found.
         """
-        self.sentence_tokens = nltk.pos_tag(sentence)
-        self.sentence_number = number
-        self.offensive_categories = []
-        self.weakly_offensive_words = defaultdict(int)
-        self.strongly_offensive_words = defaultdict(int)
-        self.number_of_clean_words = 0
-        self.number_of_offensive_words = 0
+        self.sentence_tokens = nltk.pos_tag(
+            sentence)  # List of tuples in the form of (word, part of speech tag) in the sentence.
+        self.sentence_number = number  # Location of the sentence within the original text.
+        self.offensive_categories = []  # List containing the offensive categories met by this sentence
+        self.weakly_offensive_words = dict()  # Dictionary containing weakly offensive word/category pairs
+        # and the number of occurrences in the sentence.
+        self.strongly_offensive_words = dict()  # Dictionary containing strongly offensive word/category
+        # pairs and the number of occurrences in the sentence.
+        self.number_of_clean_words = 0  # Number of clean words within the sentence.
+        self.number_of_offensive_words = 0  # Number of offensive words within the sentence.
+        self.initialize_word_dictionaries()
 
     def __str__(self):
         """
-        To string function
+        Overwrite to string function
         :return: A string giving information about the sentence.
         """
-        return 'Sent {}:{}'.format(self.sentence_number, self.sentence_tokens)
+        string = 'Sent {}: {}\n'.format(self.sentence_number, self.sentence_tokens)
+        string += '  Total Clean Words: {}'.format(self.number_of_clean_words)
+        string += '  Total Offensive Words: {}\n'.format(self.number_of_offensive_words)
+        string += '  Strongly Offensive Words: {}\n'.format(self.strongly_offensive_words)
+        string += '  Weakly Offensive Words: {}\n'.format(self.weakly_offensive_words)
+        string += '  Offensive Categories: {}\n'.format(self.offensive_categories)
+        return string
+
+    def initialize_word_dictionaries(self):
+        """
+        Initialize the weakly_offensive_words dictionary and strongly_offensive_words dictionary to have the keys
+        be the categories name and each value is another dictionary that will store the word and word counts in the
+        appropriate category.
+        :return: None.
+        """
+        for category in model_helper.get_categories():
+            self.weakly_offensive_words['{}'.format(category.name)] = dict()
+            self.strongly_offensive_words['{}'.format(category.name)] = dict()
 
     def set_clean_words(self, number: int):
         """
@@ -82,7 +92,10 @@ class Sentence:
         :param category: The category in which the word is strongly offensive.
         :return: None
         """
-        self.strongly_offensive_words['{}:{}'.format(word, category)] += 1
+        try:
+            self.strongly_offensive_words[category][word] += 1
+        except KeyError:
+            self.strongly_offensive_words[category][word] = 1
         if category not in self.offensive_categories:
             self.offensive_categories.append(category)
 
@@ -93,7 +106,10 @@ class Sentence:
         :param category: The category in which the word is weakly offensive.
         :return: None
         """
-        self.weakly_offensive_words['{}:{}'.format(word, category)] += 1
+        try:
+            self.weakly_offensive_words[category][word] += 1
+        except KeyError:
+            self.weakly_offensive_words[category][word] = 1
 
     def get_sentence_features(self):
         """
@@ -117,27 +133,19 @@ class Sentence:
         This function derives lexical features from the tokenized sentence.
         :return: a set of lexical features.
         """
-        non_offensive_words = 0
-        offensive_words = 0
-
         for word, POS_tag in self.sentence_tokens:
-            try:
-                query = Word.objects.get(word=word)  # Throws a DoesNotExist exception if the word is not offensive.
-                offensive_words += 1
-
-                for word_cat in query.word_category_set.all():  # Check if the word is strongly or weakly offensive for
-                    # each category.
-                    if word_cat.strong:
-                        self.add_strongly_offensive_word(word=word, category=word_cat.category.category)
+            off_word = model_helper.get_word(word=word)
+            if not off_word:  # Not an offensive word
+                # Update the total number of clean words in the sentence.
+                self.number_of_clean_words += 1
+            else:
+                # Update the total number of strongly offensive words in the sentence.
+                self.number_of_offensive_words += 1
+                for word_cat in off_word.get_word_features():  # each category.
+                    if word_cat['strength']:  # Check if the word is strongly or weakly offensive
+                        self.add_strongly_offensive_word(word=word, category=word_cat['category'])
                     else:
-                        self.add_weakly_offensive_word(word=word, category=word_cat.category.category)
-            except Word.DoesNotExist:
-                non_offensive_words += 1
-        # Update the total number of clean and offensive words in the sentence.
-        self.set_clean_words(non_offensive_words)
-        self.set_offensive_words(offensive_words)
-
-        return self.get_sentence_features()
+                        self.add_weakly_offensive_word(word=word, category=word_cat['category'])
 
     def extract_syntactic_features(self):
         """
