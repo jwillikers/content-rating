763d778931c3cc3987a2a70c41d602ba6811bc35
Jordan Williams <jordanwilliams@solutionary.com>
2018-04-07 10:51:36 -0500

    added tests for WordAndFeature query function

--- capstoneproject/models/raw_querysets/dictionary_raw_queryset.py
+++ capstoneproject/models/raw_querysets/dictionary_raw_queryset.py
@@ -1,21 +1,30 @@
-from django.db.models import RawQuerySet
+from django.db import connection
 
 
-class DictionaryRawQuerySet(RawQuerySet):
-    def dictfetchall(cursor):
-        "Return all rows from a cursor as a dict"
-        columns = [col[0] for col in cursor.description]
-        return [dict(zip(columns, row))
-                for row in cursor.fetchall()]
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row))
+            for row in cursor.fetchall()]
 
-    def words_and_features(self, user_id=None,
-                           category_id=None, strength=None):
-        query_string = '''
+
+def namedtuplefetchall(cursor):
+    "Return all rows from a cursor as a namedtuple"
+    from collections import namedtuple
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
+
+
+def words_and_features(user_id=None,
+                       category_id=None, strength=None):
+    """All Words and their Features belonging to Users."""
+    query_string = '''
 SELECT
-    w.id AS word_id
-    w.name AS word
-    f.category_id
-    f.strength
+    w.id AS word_id,
+    w.name AS word,
+    f.category_id,
+    f.strength,
     f.weight
 
 FROM capstoneproject_userstorage AS u
@@ -29,35 +38,35 @@ INNER JOIN capstoneproject_wordfeature AS f
     ON uf.wordfeature_id = f.id
 
 '''
-        first = True
-        query_variables = list()
+    first = True
+    query_variables = list()
 
-        if user_id:
-            query_variables.append(user_id)
-            if first:
-                query_string += '\nWHERE'
-                first = False
-            else:
-                query_string += '\nAND'
-            query_string += ' uw.userstorage_id = %s'
-        elif category_id:
-            query_variables.append(category_id)
-            if first:
-                query_string += '\nWHERE'
-                first = False
-            else:
-                query_string += '\nAND'
-            query_string += ' f.category_id = %s'
-        elif strength:
-            query_variables.append(strength)
-            if first:
-                query_string += '\nWHERE'
-                first = False
-            else:
-                query_string += '\nAND'
-            query_string += ' f.strength = %s'
+    if user_id:
+        query_variables.append(user_id)
+        if first:
+            query_string += '\nWHERE'
+            first = False
+        else:
+            query_string += '\nAND'
+        query_string += ' uw.userstorage_id = %s'
+    if category_id:
+        query_variables.append(category_id)
+        if first:
+            query_string += '\nWHERE'
+            first = False
+        else:
+            query_string += '\nAND'
+        query_string += ' f.category_id = %s'
+    if strength:
+        query_variables.append(strength)
+        if first:
+            query_string += '\nWHERE'
+            first = False
+        else:
+            query_string += '\nAND'
+        query_string += ' f.strength = %s'
 
-        with connection.cursor() as cursor:
-            cursor.execute(query_string, query_variables)
-            rows = dictfetchall(cursor)
-        return rows
+    with connection.cursor() as cursor:
+        cursor.execute(query_string, query_variables)
+        rows = dictfetchall(cursor)
+    return rows
---
+++ capstoneproject/tests/test_models/test_raw_querysets/test_dictionary_raw_queryset.py
@@ -0,0 +1,411 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from capstoneproject.models.raw_querysets.dictionary_raw_queryset \
+    import words_and_features
+from capstoneproject.models.models.category import Category
+from capstoneproject.models.models.word import Word
+from capstoneproject.models.models.word_feature import WordFeature
+from capstoneproject.models.models.user_storage import UserStorage
+
+
+class WordAndFeatureTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cat1 = Category.categories.create(
+            name='test_category1', weight=1, default=True)
+        cls.cat2 = Category.categories.create(
+            name='test_category2', weight=2, default=True)
+        cls.feature1 = WordFeature.word_features.create(
+            default=True, category=cls.cat1, strength=True, weight=1)
+        cls.feature2 = WordFeature.word_features.create(
+            default=True, category=cls.cat2, strength=False, weight=3)
+        cls.feature4 = WordFeature.word_features.create(
+            default=True, category=cls.cat1, strength=True, weight=2)
+        cls.feature5 = WordFeature.word_features.create(
+            default=False, category=cls.cat1, strength=False, weight=1)
+        cls.word1 = Word.words.create(name='word1', default=True)
+        cls.word1.word_features.add(cls.feature1.id)
+        cls.word2 = Word.words.create(name='word2', default=True)
+        cls.word2.word_features.add(cls.feature2.id)
+        cls.word3 = Word.words.create(name='word3', default=True)
+        cls.word3.word_features.add(cls.feature1.id)
+        cls.word3.word_features.add(cls.feature2.id)
+        cls.word4 = Word.words.create(name='word4', default=True)
+        cls.word4.word_features.add(cls.feature4.id)
+        cls.word5 = Word.words.create(name='word5', default=False)
+        cls.word5.word_features.add(cls.feature5.id)
+        cls.word1.save()
+        cls.word2.save()
+        cls.word3.save()
+        cls.word4.save()
+        cls.word5.save()
+        cls.user1 = User.objects.create_user(
+            username='user1', password='12345')
+        cls.user1.save()
+        cls.user2 = User.objects.create_user(
+            username='user2', password='12346')
+        cls.user2.save()
+        cls.user_storage1 = UserStorage.user_storage.get(user=cls.user1)
+        cls.user_storage1.words.add(cls.word1)
+        cls.user_storage1.words.add(cls.word2)
+        cls.user_storage1.words.add(cls.word3)
+        cls.user_storage1.categories.add(cls.cat1)
+        cls.user_storage1.categories.add(cls.cat2)
+        cls.user_storage1.word_features.add(cls.feature1)
+        cls.user_storage1.word_features.add(cls.feature2)
+        cls.user_storage1.save()
+        cls.user_storage2 = UserStorage.user_storage.get(user=cls.user2)
+        cls.user_storage2.words.add(cls.word1)
+        cls.user_storage2.words.add(cls.word2)
+        cls.user_storage2.words.add(cls.word3)
+        cls.user_storage2.words.add(cls.word5)
+        cls.user_storage2.categories.add(cls.cat1)
+        cls.user_storage2.categories.add(cls.cat2)
+        cls.user_storage2.word_features.add(cls.feature1)
+        cls.user_storage2.word_features.add(cls.feature2)
+        cls.user_storage2.word_features.add(cls.feature5)
+        cls.user_storage2.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        Category.categories.all().delete()
+        Word.words.all().delete()
+        WordFeature.word_features.all().delete()
+
+    def setUp(self):
+        self.user1 = WordAndFeatureTestCase.user1
+        self.user_storage1 = WordAndFeatureTestCase.user_storage1
+        self.user2 = WordAndFeatureTestCase.user2
+        self.user_storage2 = WordAndFeatureTestCase.user_storage2
+        self.word1 = WordAndFeatureTestCase.word1
+        self.word2 = WordAndFeatureTestCase.word2
+        self.word3 = WordAndFeatureTestCase.word3
+        self.word4 = WordAndFeatureTestCase.word4
+        self.word5 = WordAndFeatureTestCase.word5
+        self.cat1 = WordAndFeatureTestCase.cat1
+        self.cat2 = WordAndFeatureTestCase.cat2
+        self.feature1 = WordAndFeatureTestCase.feature1
+        self.feature2 = WordAndFeatureTestCase.feature2
+        self.feature4 = WordAndFeatureTestCase.feature4
+        self.feature5 = WordAndFeatureTestCase.feature5
+
+    def test_args_none(self):
+        results = words_and_features()
+        self.assertIsInstance(results, list, msg='result is not a list')
+        self.assertGreaterEqual(len(results), 3)
+        self.assertIsInstance(
+            results[0], dict,
+            msg='result is not a list of dictionaries')
+        self.assertIn(
+            {'word_id': self.word1.id,
+             'word': self.word1.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word1')
+        self.assertIn(
+            {'word_id': self.word2.id,
+             'word': self.word2.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results, msg='missing word2')
+        self.assertIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word3, feature1')
+        self.assertIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results, msg='missing word3, feature2')
+        self.assertNotIn(
+            {'word_id': self.word4.id,
+             'word': self.word4.name,
+             'category_id': self.feature4.category,
+             'strength': self.feature4.strength,
+             'weight': self.feature4.weight},
+            results, msg='contains word4 which is not linked to a user')
+        self.assertIn(
+            {'word_id': self.word5.id,
+             'word': self.word5.name,
+             'category_id': self.feature5.category.id,
+             'strength': self.feature5.strength,
+             'weight': self.feature5.weight},
+            results, msg='missing word5')
+
+    def test_arg_user_id(self):
+        results = words_and_features(user_id=self.user1.id)
+        self.assertIsInstance(results, list, msg='result is not a list')
+        self.assertGreaterEqual(len(results), 3)
+        self.assertIsInstance(
+            results[0], dict,
+            msg='result is not a list of dictionaries')
+        self.assertIn(
+            {'word_id': self.word1.id,
+             'word': self.word1.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word1')
+        self.assertIn(
+            {'word_id': self.word2.id,
+             'word': self.word2.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results, msg='missing word2')
+        self.assertIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word3, feature1')
+        self.assertIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results, msg='missing word3, feature2')
+        self.assertNotIn(
+            {'word_id': self.word4.id,
+             'word': self.word4.name,
+             'category_id': self.feature4.category,
+             'strength': self.feature4.strength,
+             'weight': self.feature4.weight},
+            results, msg='contains word4 which is not linked to this user')
+        self.assertNotIn(
+            {'word_id': self.word5.id,
+             'word': self.word5.name,
+             'category_id': self.feature5.category.id,
+             'strength': self.feature5.strength,
+             'weight': self.feature5.weight},
+            results, msg='should not contain word5 which belongs to user2')
+
+    def test_arg_category_id(self):
+        results = words_and_features(category_id=self.cat1.id)
+        self.assertIsInstance(results, list, msg='result is not a list')
+        self.assertGreaterEqual(len(results), 3)
+        self.assertIsInstance(
+            results[0], dict,
+            msg='result is not a list of dictionaries')
+        self.assertNotIn(
+            {'word_id': self.word4.id,
+             'word': self.word4.name,
+             'category_id': self.feature4.category,
+             'strength': self.feature4.strength,
+             'weight': self.feature4.weight},
+            results, msg='contains word4 which is not linked to this user')
+        self.assertIn(
+            {'word_id': self.word1.id,
+             'word': self.word1.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word1')
+        self.assertNotIn(
+            {'word_id': self.word2.id,
+             'word': self.word2.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results,
+            msg='contains category 2 word when it should not')
+        self.assertIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word3, feature1')
+        self.assertNotIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results,
+            msg='contains category 2 feature in word 3 when it should not')
+        self.assertNotIn(
+            {'word_id': self.word4.id,
+             'word': self.word4.name,
+             'category_id': self.feature4.category,
+             'strength': self.feature4.strength,
+             'weight': self.feature4.weight},
+            results, msg='contains word4 which is not linked to a user')
+        self.assertIn(
+            {'word_id': self.word5.id,
+             'word': self.word5.name,
+             'category_id': self.feature5.category.id,
+             'strength': self.feature5.strength,
+             'weight': self.feature5.weight},
+            results,
+            msg='missing word5')
+
+    def test_arg_strength(self):
+        results = words_and_features(strength=True)
+        self.assertIsInstance(results, list, msg='result is not a list')
+        self.assertGreaterEqual(len(results), 3)
+        self.assertIsInstance(
+            results[0], dict,
+            msg='result is not a list of dictionaries')
+        self.assertIn(
+            {'word_id': self.word1.id,
+             'word': self.word1.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word1')
+        self.assertNotIn(
+            {'word_id': self.word2.id,
+             'word': self.word2.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results,
+            msg='contains word2 of Strength = False when it should not')
+        self.assertIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word3, feature1')
+        self.assertNotIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results,
+            msg='''contains Strength = False feature in word3 when it should not
+            ''')
+        self.assertNotIn(
+            {'word_id': self.word5.id,
+             'word': self.word5.name,
+             'category_id': self.feature5.category.id,
+             'strength': self.feature5.strength,
+             'weight': self.feature5.weight},
+            results,
+            msg='contains word5 of Strength = False when it should not')
+
+    def test_arg_user_id_and_category_id(self):
+        results = words_and_features(
+            user_id=self.user1.id,
+            category_id=self.cat1.id)
+        self.assertIsInstance(results, list, msg='result is not a list')
+        self.assertGreaterEqual(len(results), 3)
+        self.assertIsInstance(
+            results[0], dict,
+            msg='result is not a list of dictionaries')
+        self.assertNotIn(
+            {'word_id': self.word4.id,
+             'word': self.word4.name,
+             'category_id': self.feature4.category,
+             'strength': self.feature4.strength,
+             'weight': self.feature4.weight},
+            results, msg='contains word4 which is not linked to this user')
+        self.assertIn(
+            {'word_id': self.word1.id,
+             'word': self.word1.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word1')
+        self.assertNotIn(
+            {'word_id': self.word2.id,
+             'word': self.word2.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results,
+            msg='contains category 2 word when it should not')
+        self.assertIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word3, feature1')
+        self.assertNotIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results,
+            msg='contains category 2 feature in word 3 when it should not')
+        self.assertNotIn(
+            {'word_id': self.word4.id,
+             'word': self.word4.name,
+             'category_id': self.feature4.category,
+             'strength': self.feature4.strength,
+             'weight': self.feature4.weight},
+            results, msg='contains word4 which is not linked to this user')
+        self.assertNotIn(
+            {'word_id': self.word5.id,
+             'word': self.word5.name,
+             'category_id': self.feature5.category.id,
+             'strength': self.feature5.strength,
+             'weight': self.feature5.weight},
+            results,
+            msg='contains word5 that does not belong to user1')
+
+    def test_args_all(self):
+        results = words_and_features(
+            user_id=self.user1.id,
+            category_id=self.cat1.id,
+            strength=self.feature1.strength)
+        self.assertIsInstance(results, list, msg='result is not a list')
+        self.assertGreaterEqual(len(results), 3)
+        self.assertIsInstance(
+            results[0], dict,
+            msg='result is not a list of dictionaries')
+        self.assertIn(
+            {'word_id': self.word1.id,
+             'word': self.word1.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word1')
+        self.assertNotIn(
+            {'word_id': self.word2.id,
+             'word': self.word2.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results,
+            msg='''contains word2 of Strength = False and category2 when it should not
+            ''')
+        self.assertIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat1.id,
+             'strength': self.feature1.strength,
+             'weight': self.feature1.weight},
+            results, msg='missing word3, feature1')
+        self.assertNotIn(
+            {'word_id': self.word3.id,
+             'word': self.word3.name,
+             'category_id': self.cat2.id,
+             'strength': self.feature2.strength,
+             'weight': self.feature2.weight},
+            results,
+            msg='''contains Strength = False and category2 feature in word3 when it should not
+            ''')
+        self.assertNotIn(
+            {'word_id': self.word5.id,
+             'word': self.word5.name,
+             'category_id': self.feature5.category.id,
+             'strength': self.feature5.strength,
+             'weight': self.feature5.weight},
+            results,
+            msg='''contains word5 of Strength = False that does not belong
+            to user1''')
