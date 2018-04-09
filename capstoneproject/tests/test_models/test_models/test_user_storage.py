9b5eea7cb52c6ec00258ce72de027973e2e53721
Jordan Williams <jordanwilliams@solutionary.com>
2018-04-09 16:59:35 -0500

    added fixture for UserStorage

--- capstoneproject/tests/test_models/test_models/test_user_storage.py
+++ capstoneproject/tests/test_models/test_models/test_user_storage.py
@@ -7,29 +7,35 @@ from capstoneproject.models.models.word_feature import WordFeature
 
 
 class UserStorageTestClass(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user1', password='123456')
+        self.user.save()
+        self.user_storage = UserStorage.user_storage.get(user=self.user.id)
+
+    def tearDown(self):
+        self.user_storage.delete()
+        self.user.delete()
+
     def test_autocreate(self):
-        user = User.objects.create(username='user1', password='123456')
-        user.save()
-        user_storage = UserStorage.user_storage.get(user=user.id)
         self.assertIsNotNone(
-            user_storage,
+            self.user_storage,
             msg="UserStorage with user's pk was not created")
         default_words = list(Word.words.filter(default=True).all())
-        user_words = list(user_storage.words.all())
+        user_words = list(self.user_storage.words.all())
         self.assertListEqual(
             user_words,
             default_words,
             msg="User's Words do not match default Words")
         default_categories = list(Category.categories.filter(
             default=True).all())
-        user_categories = list(user_storage.categories.all())
+        user_categories = list(self.user_storage.categories.all())
         self.assertListEqual(
             user_categories,
             default_categories,
             msg="User's categories do not match default Categories")
         default_features = list(WordFeature.word_features.filter(
             default=True).all())
-        user_features = list(user_storage.word_features.all())
+        user_features = list(self.user_storage.word_features.all())
         self.assertListEqual(
             user_features,
             default_features,
