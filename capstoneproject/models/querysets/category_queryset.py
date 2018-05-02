3452791b9af9b74e56c2d6bd6ccd64400fd7faed
Jordan Williams <jordanwilliams@solutionary.com>
2018-05-02 15:01:28 -0500

    Documented category_queryset module

--- capstoneproject/models/querysets/category_queryset.py
+++ capstoneproject/models/querysets/category_queryset.py
@@ -2,10 +2,21 @@ from django.db.models import QuerySet
 
 
 class CategoryQuerySet(QuerySet):
+    """Category model QuerySet for simplifying queries."""
+
     def default(self):
+        """
+        Filter the QueryString to only include default values.
+        :return: A CategoryQuerySet containing only default Categories.
+        """
         return self.filter(default=True)
 
     def of_user(self, user_id):
+        """
+        Filter the QueryString to only include this User's Categories.
+        :param user_id: Id of the User model instance.
+        :return: A Raw CategoryQuerySet containing only the User's Categories.
+        """
         from capstoneproject.models.models.category import Category
         return self.raw('''
 SELECT
